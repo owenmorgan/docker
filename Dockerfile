d4c56f64af46989283bf7c7346f0a6b5a7b275ff
Owen Morgan <owen.morgan@oneiota.co.uk>
2016-08-24 17:23:21 +0100

    added methods to get required detail.

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+from scratch
+# this is a test image to test labels.
+LABEL {"applicationName" : "hello-app","portMapping" : [8080:80]}
--- main.go
+++ main.go
@@ -1,26 +1,100 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecr"
 	docker "github.com/fsouza/go-dockerclient"
 )
 
-var endpoint string = "unix:///var/run/docker.sock"
+const imageConfigKey string = "uk.co.oneiota-config"
 
-func main() {
-	client, _ := docker.NewClient(endpoint)
-	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
-	for _, img := range imgs {
-		fmt.Println("ID: ", img.ID)
-		fmt.Println("RepoTags: ", img.RepoTags)
-		fmt.Println("Created: ", img.Created)
-		fmt.Println("Size: ", img.Size)
-		fmt.Println("VirtualSize: ", img.VirtualSize)
-		fmt.Println("ParentId: ", img.ParentID)
+// ImageConfig - which is on the label
+type ImageConfig struct {
+	AppName     string   `json:"applicationName"`
+	PortMapping []string `json:"portMapping"`
+}
+
+// LlongDockerClient - client to do all the things
+type LlongDockerClient struct {
+	dockerCli      *docker.Client
+	ecrSvs         *ecr.ECR
+	dockerAuth     docker.AuthConfiguration
+	localEndpoint  string
+	remoteEndpoint string
+}
+
+// NewLlongDockerClient - returns a new instance of a LlongDockerClient
+func NewLlongDockerClient(awsRegion string, localEndpoint string, remoteEnpoint string, awsUsername string, awsPassword string) *LlongDockerClient {
+	ecrSvs := ecr.New(session.New(), &aws.Config{Region: aws.String(awsRegion)})
+	var err error
+	dockerCli, err := docker.NewClient(localEndpoint)
+	if err != nil {
+		panic(err)
+	}
+	dockerAuth := docker.AuthConfiguration{
+		Username:      awsUsername,
+		Password:      awsPassword,
+		ServerAddress: remoteEnpoint,
+	}
+	return &LlongDockerClient{
+		dockerCli:  dockerCli,
+		ecrSvs:     ecrSvs,
+		dockerAuth: dockerAuth,
 	}
 }
 
-func Pull(image string) {
+// Docker -
+type Docker interface {
+	GetRepoImages(repoName string) *ecr.ListImagesOutput
+	GetImageConfig(image string) *ImageConfig
+	getImageLabels(image string, tag string) map[string]string
+}
 
+// GetRepoImages - wrapper for aws ecs to get repo images.
+func (llong *LlongDockerClient) GetRepoImages(repoName string) (*ecr.ListImagesOutput, error) {
+	listImagesImp := ecr.ListImagesInput{
+		RepositoryName: &repoName,
+	}
+	resp, err := llong.ecrSvs.ListImages(&listImagesImp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// GetImageConfig - returns image config from the label
+func (llong *LlongDockerClient) GetImageConfig(image string, tag string) (ImageConfig, error) {
+	tags := llong.getImageLabels(image, tag)
+	retIC := ImageConfig{}
+	var retErr error
+	if cStr, ok := tags[imageConfigKey]; ok {
+		cByte := []byte(cStr)
+		retErr := json.Unmarshal(cByte, &retIC)
+		if retErr != nil {
+			return retIC, retErr
+		}
+	}
+	return retIC, retErr
+}
+
+func (llong *LlongDockerClient) getImageLabels(image string, tag string) map[string]string {
+	returnMap := make(map[string]string)
+	fullImagePath := image + ":" + tag
+	pullOpts := docker.PullImageOptions{
+		Repository: image,
+		Tag:        tag,
+	}
+	err := llong.dockerCli.PullImage(pullOpts, llong.dockerAuth)
+	if err != nil {
+		return returnMap
+	}
+	imageMeta, err := llong.dockerCli.InspectImage(fullImagePath)
+	if err != nil {
+		return returnMap
+	}
+	go llong.dockerCli.RemoveImageExtended(fullImagePath, docker.RemoveImageOptions{Force: true})
+	return imageMeta.Config.Labels
 }
